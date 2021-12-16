05372090c5d4c1548f35f191fa29433145e82b67
Claudia Scarbrough <rubyharrts@gmail.com>
2021-12-15 21:02:21 -0600

    done

---
+++ user-onboarding/src/validation/formSchema.js
@@ -0,0 +1,20 @@
+import * as yup from "yup";
+
+const formSchema = yup.object().shape({
+  username: yup
+    .string()
+    .trim()
+    .required("Username is required!")
+    .min(3, "Must be 3 or more letters"),
+  email: yup
+    .string()
+    .email("Must be a valid email address")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(6, "Password must be 6 characters long"),
+  tos: yup.boolean().oneOf([true], "Must accept the terms and conditions"),
+});
+
+export default formSchema;
